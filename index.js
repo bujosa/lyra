ab0a38ec4cdd3872593f25f9333de55d2773ad7e
David Bujosa <davidbujosa@gmail.com>
2022-03-13 14:27:48 +0100

    feat: initial setup lyra

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.CLIENT_TOKEN);
